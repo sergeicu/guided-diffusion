d567531e84975f14351f4969af58a8e3a16a8e4d
Serge Vasylechko <serge.vasylechko@childrens.harvard.edu>
2024-02-29 13:01:46 -0500

    added description of results of 3D training

--- exp1.sh
+++ exp1.sh
@@ -8,3 +8,18 @@ cat /tmp/openai-2024-02-28-05-12-16-486120/log.txt
 
 # test 
 python scripts/image_sample.py
+
+# inferrence results: 
+ls /fileserver/Rad-Warfield-e2/Groups/Imp-Recons/serge/code/diffusion/experiments/s20240209_oecorr/guided-diffusion/trained/openai-2024-02-28-05-25-58-668429/model070500
+- 3D diffusion training with ~100 subjects does NOT work. 
+- generated images are a gargle. 
+- possible reasons of failure: 
+    - not enough samples? 
+    - the 3D network is too shallow to generate in 3D from noise? (possibly yes)
+- future work: 
+    - try to train a 2D network instead on cardiac images... (split my dataset into pngs...)
+    - verify if 2D generation works... 
+    - if it does -> this will support my hypothesis that 3D generation is too hard to generate. 
+
+- how long will it take? 
+    - 2hrs? 
